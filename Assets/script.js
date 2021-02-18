f9fef42e68835d7a9f14c5d3eda66167121d4ef9
Adam <a3aranha@gmail.com>
2021-02-17 19:02:47 -0500

    Figuring out CORS issues

--- Assets/script.js
+++ Assets/script.js
@@ -37,11 +37,11 @@ async function getWeatherToday(query) {
     iconToday = weatherToday.weather[0].icon
 
 
-    uvToday = await fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,hourly,minutely&appid=d527e56ccdc4853efdf6570164c6eeab`).then(r => r.json())
+    uvToday = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,hourly,minutely&appid=d527e56ccdc4853efdf6570164c6eeab`).then(r => r.json())
 
     uv = uvToday.current.uvi
 
-    weatherWeek = await fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=${query}&units=metric&appid=d527e56ccdc4853efdf6570164c6eeab`).then(r => r.json())
+    weatherWeek = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&units=metric&appid=d527e56ccdc4853efdf6570164c6eeab`).then(r => r.json())
     workingWeek = weatherWeek.list.filter(test => test.dt_txt.endsWith('12:00:00'))
 
     document.querySelector('#weatherRow').innerHTML = ''
